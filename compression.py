0ee90691d0e247e45ba4e94ae98f9bfcbcb6d503
danylo.kolinko <kolinko@ucu.edu.ua>
2019-05-13 15:44:53 +0300

    Fix HoG, add bad visualization

--- compression.py
+++ compression.py
@@ -1,19 +1,22 @@
-from PIL import Image
+import cv2
+
+from PIL import Image, ImageFilter
 from math import floor, ceil, sin, pi, sqrt, atan2, degrees
 from numpy import array, full, zeros
 import numpy
-import matplotlib.pyplot as plt
-import random
+import matplotlib
+
 
 class Compressor:
     default_size = (96, 96)
     constant = 2  # different constant k will corespond to interpolation Lanczos_k
-    x = [1, 0, 1]
     Sobel_dx_matrix = array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
     Sobel_dy_matrix = Sobel_dx_matrix.transpose()
 
     def __init__(self, path):
         self.image = Image.open(path)
+        self.array_image = numpy.asarray(self.image)
+
         self.rate = self.image.size[0] / Compressor.default_size[0]
         self.Lanczos_constant = round(self.constant * self.rate)
 
@@ -39,8 +42,8 @@ class Compressor:
     def lanczos_execution(self):
         new_image = Image.new("L", Compressor.default_size)
         pixels = new_image.load()
-        for i in range(new_image.size[0]):
-            for j in range(new_image.size[1]):
+        for i in range(self.default_size[0]):
+            for j in range(self.default_size[1]):
                 pixels[i, j] = round(
                     self.lanczos_resampling(round(self.rate * (i + 0.5)), round(self.rate * (j + 0.5))))
         new_image.save("res.png")
@@ -68,36 +71,47 @@ class Compressor:
         for i in range(X_0 - 1, X_0 + 2):
             for j in range(Y_0 - 1, Y_0 + 2):
                 try:
-                    current_pixel = self.image.getpixel((i, j))
+                    current_pixel = self.array_image[i, j]
+                    current_pixel = sqrt(current_pixel)  # gamma-correction
                 except Exception as e:
-                    continue
+                    return (0,0)
                 resx += self.__gradient_kernel__(X_0 - i, Y_0 - j, Compressor.Sobel_dx_matrix) * current_pixel
                 resy += self.__gradient_kernel__(X_0 - i, Y_0 - j, Compressor.Sobel_dy_matrix) * current_pixel
-        magnitude = sqrt(resx ** 2 + resy ** 2)
-        angle = abs(degrees(atan2(resx, resy)))
-        return (magnitude, angle)
+
+        return (resx, resy)
 
     def gradient_execution(self):
-        magnitude = full(self.default_size, .0)
-        angle = full(self.default_size, .0)
+        dx = full(self.default_size, .0)
+        dy = full(self.default_size, .0)
         for i in range(Compressor.default_size[0]):
             for j in range(Compressor.default_size[1]):
                 new = self.gradient_convolution(i, j)
-                print(new[0])
-                magnitude[i, j] = new[0]
-                angle[i, j] = new[1]
+
+                dx[i, j] = new[0]
+                dy[i, j] = new[1]
+
+        magnitude = numpy.sqrt(dx ** 2 + dy ** 2)
+        angle = (numpy.arctan2(dy, dx)) * 180 / pi
+        a = self.matrix2image(magnitude)
+        b = self.matrix2image(angle)
+        c = self.matrix2image(dx)
+        d = self.matrix2image(dy)
+        a.save("a.png")
+        b.save("b.png")
+        c.save("c.png")
+        d.save("d.png")
         return (magnitude, angle)
 
     def matrix2image(self, mat):
-        new = Image.new("L", self.default_size)
+        new = Image.new("L", mat.shape)
         pixels = new.load()
-        for i in range(self.default_size[0]):
-            for j in range(self.default_size[1]):
-                pixels[i,j] = int(mat[i,j])
+        for i in range(mat.shape[0]):
+            for j in range(mat.shape[1]):
+                pixels[j, i] = int(mat[i, j])
+        return new
 
-        new.save("gradient.png")
     def HoG__calculation(self, magnitude_matrix, angle_matrix):
-        HoG_arrays = zeros((self.default_size[0]//8, self.default_size[1]//8, 9), dtype=float)
+        HoG_arrays = zeros((self.default_size[0] // 8, self.default_size[1] // 8, 9), dtype=float)
         for i in range(self.default_size[0] // 8):
             for j in range(self.default_size[1] // 8):
                 norm2 = 0
@@ -107,127 +121,108 @@ class Compressor:
                         current = angle_matrix[cind[0], cind[1]]
 
                         weight = (current % 20) / 20
-                        print(weight)
-                        HoG_arrays[i, j, (int(current) // 20)%9] = weight * magnitude_matrix[cind[0], cind[1]]
-                        HoG_arrays[i, j, (1 + int(current - .01) // 20)%9] = (1 - weight) * magnitude_matrix[cind[0], cind[1]]
+                        HoG_arrays[i, j, (int(current) // 20) % 9] = weight * magnitude_matrix[cind[0], cind[1]]
+                        HoG_arrays[i, j, (1 + int(current) // 20) % 9] = (1 - weight) * magnitude_matrix[
+                            cind[0], cind[1]]
 
-                for k in range(9):
-                    norm2 += HoG_arrays[i, j, k] ** 2
 
-                norm2 = sqrt(norm2)
-                for k in range(9):
-                    HoG_arrays[i, j, k] /= norm2
+
+        orientations = 9
+        s_row, s_col = (96, 96)
+        c_row, c_col = (8, 8)
+        b_row, b_col = (2,2)
+        n_cells_row = int(s_row // c_row)  # number of cells along row-axis
+        n_cells_col = int(s_col // c_col)
+        n_blocks_row = (n_cells_row - b_row) + 1
+        n_blocks_col = (n_cells_col - b_col) + 1
+        normalized_blocks = numpy.zeros((n_blocks_row, n_blocks_col,
+                                         b_row, b_col, orientations))
+        eps = 0.0001
+
+        for r in range(n_blocks_row):
+            for c in range(n_blocks_col):
+                block = HoG_arrays[r:r + b_row, c:c + b_col, :]
+                out = block / numpy.sqrt(numpy.sum(block ** 2) + eps ** 2)
+                out = numpy.minimum(out, 0.2)
+                out = out / numpy.sqrt(numpy.sum(out ** 2) + eps ** 2)
+                normalized_blocks[r, c, :] = out
         return HoG_arrays.ravel()
 
 
-"""
-        csx = 8
-        csy = 8
 
-        max_angle = numpy.pi
-        n_cells_y, n_cells_x, nbins = HoG_arrays.shape
-        sx, sy = n_cells_x * csx, n_cells_y * csy
-        plt.style.use('dark_background')
-        plt.close()
-        plt.figure()  # figsize=(sx/2, sy/2))#, dpi=1)
-        plt.xlim(0, sx)
-        plt.ylim(sy, 0)
-
-        center = csx // 2, csy // 2
-        b_step = max_angle / nbins
-
-
-        for i in range(Compressor.default_size[0]//8):
-            for j in range(Compressor.default_size[1]//8):
-                for k in range(nbins):
-                    if HoG_arrays[i, j, k] != 0:
-                        length = 1 * HoG_arrays[i, j, k]
-                        plt.arrow((center[0] + j * csx) - numpy.cos(b_step * k) * (center[0] - 1),
-                                  (center[1] + i * csy) + numpy.sin(b_step * k) * (center[1] - 1),
-                                  2 * numpy.cos(b_step * k) * (center[0] - 1), - 2 * numpy.sin(b_step * k) * (center[1] - 1),
-                                  width=length, color=  str(1 - length),  # 'black',
-                                  head_width=2.2*length, head_length= 2.2*length,
-                                  length_includes_head=True)
-
-        plt.show()
-
-        r = HoG_arrays.ravel()
-        print(r.max())
-        return HoG_arrays.ravel()
-
-    def image_gen(self, size):
-        new = Image.new("L", size)
-        pixels = new.load()
-        for i in range(size[0]):
-            for j in range(size[1]):
-                pixels[i, j] = random.randint(0, 255)
-        new.save("im.png")
-
-c1 = Compressor("im.png")
-c1.image_gen((96, 96))
-c = Compressor("im.png")
-a = c.gradient_execution()
-ho = c.HoG__calculation(a[0], a[1])
-print(ho)
-from skimage.feature import hog
-pic = Image.open("im.png")
-pix = numpy.array(pic.getdata()).reshape(96, 96)
-c.whole_image_into_WB()
-res = hog(pix)
-print(len(ho), len(res))
-print(res)
-
-
-
-pic = Image.open("res.png")
-pix = numpy.array(pic.getdata()).reshape(96, 96)
-
-
-c = Compressor("res.png")
-a = c.gradient_execution()
-c.matrix2image(a[0])
-ho = c.HoG__calculation(a[0], a[1])
-from skimage.feature import hog
-
-print(ho)
-c.whole_image_into_WB()
-res = hog(pix, visualize=True)
-print(len(ho), len(res[0]))
-print(res[0])
-plt.imshow(res[1], 'gray')
 """
 
-def OLD_compress_as_mean(image_path):
-    default_size = 96
-    image_ex = Image.open(image_path)
-    new_image = Image.new("L", (default_size, default_size))
-    pixels = new_image.load()
-    region = (image_ex.size[0] / default_size, image_ex.size[1] / default_size)
+# Python gradient calculation
 
-    for w in range(default_size):
-        for h in range(default_size):
+# Read image
+im = numpy.asarray(Image.open("img/aaa.png"))
+im = numpy.float32(im) / 255.0
 
-            c_region = (w * region[0], h * region[1])
-            n_region = ((w + 1) * region[0], (h + 1) * region[1])
-            num = 0
+# Calculate gradient
 
-            for i in range(floor(c_region[0]), ceil(n_region[0])):
-                for j in range(floor(c_region[1]), ceil(n_region[1])):
+from skimage import filters
+gx = filters.sobel_v(im)
+gy = filters.sobel_h(im)
+dx = cv2.Sobel(im, cv2.CV_32F, 1, 0, ksize=1)
+dy = cv2.Sobel(im, cv2.CV_32F, 0, 1, ksize=1)
 
-                    try:
-                        ppx = image_ex.getpixel((i, j))
-                    except IndexError:
-                        continue
-
-                    num += WB_converter(rgb=ppx)  # from RGB to L
-
-            num = ceil(num / ((ceil(n_region[0]) - floor(c_region[0])) * (ceil(n_region[1]) - floor(c_region[1]))))
-            pixels[w, h] = num
-    new_image.save("old_result.jpg")
+mag, angle = cv2.cartToPolar(gx, gy, angleInDegrees=True)
+mag1, angle1 = cv2.cartToPolar(dx, dy, angleInDegrees=True)
 
 
-def WB_converter(rgb):
-    if len(rgb) == 3:
-        return rgb[0] * 299 / 1000 + rgb[1] * 587 / 1000 + rgb[2] * 114 / 1000
 
-Compressor("img/blet.png").lanczos_execution()
+
+c1 = Compressor("img/aaa.png")
+a = c1.gradient_execution()
+fffff = (a[0] - mag*numpy.max(a[0])/numpy.max(mag))
+qqqqq = (a[1] - angle*numpy.max(a[1])/numpy.max(angle))
+
+
+
+h = c1.HoG__calculation(mag, angle)
+from skimage.feature import hog
+from skimage import draw
+
+im = Image.open("img/aaa.png")
+ima = numpy.asarray(im)
+q1 = hog(ima, block_norm='L2', visualize=True)
+
+s_row, s_col = (96, 96)
+c_row, c_col = (8, 8)
+b_row, b_col = (2,2)
+orientations = 9
+
+n_cells_row = int(s_row // c_row)  # number of cells along row-axis
+n_cells_col = int(s_col // c_col)
+
+radius = min(c_row, c_col) // 2 - 1
+orientations_arr  = numpy.arange(orientations)
+# set dr_arr, dc_arr to correspond to midpoints of orientation bins
+orientation_bin_midpoints = (
+        numpy.pi * (orientations_arr + .5) / orientations)
+dr_arr = radius * numpy.sin(orientation_bin_midpoints)
+dc_arr = radius * numpy.cos(orientation_bin_midpoints)
+hog_image = numpy.zeros((s_row, s_col), dtype=float)
+for r in range(n_cells_row):
+    for c in range(n_cells_col):
+        for o, dr, dc in zip(orientations_arr, dr_arr, dc_arr):
+            centre = tuple([r * c_row + c_row // 2,
+                            c * c_col + c_col // 2])
+            rr, cc = draw.line(int(centre[0] - dc),
+                               int(centre[1] + dr),
+                               int(centre[0] + dc),
+                               int(centre[1] - dr))
+            hog_image[rr, cc] += h[r, c, o]
+
+from matplotlib import pyplot
+w = h.ravel()
+
+pyplot.imsave("bil.png", hog_image, cmap='gist_gray')
+pyplot.imsave("blaaa.png", q1[1], cmap='gist_gray')
+
+s = 0
+for i in range(1296):
+    s += q1[0][i] - w[i]
+    print(q1[0][i], w[i])
+print(s)
+"""
